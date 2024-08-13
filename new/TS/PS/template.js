63f3dd4e2dd535ef81dd0e5cc3eac652c4ca7848
kabsoumy3a <sghong@elecvery.com>
2024-08-13 14:02:01 +0900

    template

--- new/TS/PS/template.js
+++ new/TS/PS/template.js
@@ -3,17 +3,8 @@
 /////////////////////////////////////////////////////////
 
 const fs = require("fs");
-const input = fs.readFileSync(process.stdin.fd, "utf-8");
-const lines = input.split("\n");
-//lines = ["5", "1 1", "2 3", "3 4", "9 8", "5 2"];
+const lines = fs.readFileSync(process.stdin.fd).toString().trim().split("\n");
 
-
-const T = parseInt(lines[0]);
-const arr = lines.slice(1, T+1);
-const pairList = arr.map((line => line.split(" ").map(s => parseInt(s))));
-
-for(const pair of pairList){
-  const [a,b] = pair;
-  console.log(a+b);
-}
-
+//const T = parseInt(lines[0]);
+const arr = lines.slice(1).map((line => line.split(" ").map(s => +s)));
+arr.forEach(([a,b]) => console.log(a+b));
